fixed scaling issues in pca and ica

VS265-Project/util.py
@@ -183,6 +183,7 @@ def ica_reconst(images, R, n=None):
     #im_weights = im_vecs @ basis #(num_ims, n)
     im_weights = ica.mixing_ #(num_ims, n)
     reconst = im_weights @ basis.T #(num_ims, im_size)
+    reconst = np.floor( reconst + abs(reconst.min()) )#rescale
 
     return reconst.reshape(images.shape)
 
@@ -208,6 +209,6 @@ def pca_reconst(images, R, n=None):
     #im_weights = pca.mixing_ #(num_ims, n)
     im_weights = im_vecs.T @ basis #(num_ims, n)
     reconst = im_weights @ basis.T #(num_ims, im_size)
-    reconst = reconst/reconst.size #normalize
+    reconst = np.floor( reconst/reconst.size + abs(reconst.min()) ) #normalize
 
     return reconst.reshape(images.shape)